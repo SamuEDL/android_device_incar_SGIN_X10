f96fbbee27201a8968ed0761afe5c3fa0d34381c
SamuEDL <59443298+SamuEDL@users.noreply.github.com>
2023-05-27 19:47:01 +0200

    Update omni_SGIN_X10.mk

--- omni_SGIN_X10.mk
+++ omni_SGIN_X10.mk
@@ -5,16 +5,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from SGIN_X10 device
 $(call inherit-product, device/incar/SGIN_X10/device.mk)
 
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := SGIN_X10
 PRODUCT_NAME := omni_SGIN_X10
 PRODUCT_BRAND := SGIN
@@ -23,7 +20,3 @@ PRODUCT_MANUFACTURER := incar
 
 PRODUCT_GMS_CLIENTID_BASE := android-incar
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="SGIN_X10_EEA-user 11 RP1A.201005.001 20211207 release-keys"
-
-BUILD_FINGERPRINT := SGIN/SGIN_X10_EEA/SGIN_X10_EEA:11/RP1A.201005.001/20211207:user/release-keys
